c2bac617d69368102826f3872ee0646e2093fa6b
smoothoperator006 <josh.ayush2006@gmail.com>
2025-09-22 15:26:14 +0530

    question 1

---
+++ pypract/loops.py
@@ -0,0 +1,6 @@
+for i in range(2,8,2):
+  print("the number being manipulated is",i)
+i=7
+while(int(i)>0):
+  i=int(i/2)
+  print("the number being manipulate is",i)
---
+++ pypract/q1.py
@@ -0,0 +1,10 @@
+def triple_and(a,b,c):
+ if a and b and c:
+  return True
+ else:
+  return False
+ 
+num1=int(input("enter num1"))
+num2=int(input("enter num2"))
+num3=int(input("enter num3"))
+print(triple_and(num1,num2,num3))
